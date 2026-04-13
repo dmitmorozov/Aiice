238e4ce22e09bdb207909b19fe68af2a1ed35dc5
Andrey Kuznetsov <113019270+kdduha@users.noreply.github.com>
2026-04-13 21:01:41 +0300

    v0.10.0

--- src/aiice/__init__.py
+++ src/aiice/__init__.py
@@ -12,3 +12,10 @@ from aiice.benchmark import AIICE
 
 # visible modules to pdoc
 __all__ = ["AIICE", "core", "loader", "metrics", "preprocess"]
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("aiice-bench")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
